067e26a7fb36f8daaf353b5fecf93cd968199e06
Badr <baderhelal567@gmail.com>
2023-01-14 15:51:27 +0200

    d

--- config/config.js
+++ config/config.js
@@ -1,16 +1,11 @@
 
 module.exports = {
-    externalRepos: {
-        authentication: "http://localhost:4203",
-        frontend: "https://tridmark.com",
-        // frontend: "http://localhost:4200",
-    },
+
     db: {
-        uri: 'mongodb+srv://badr1002:og0vQafkZNaxwh4W@cluster0.jmhw5wr.mongodb.net/Coligo', 
-        // uri:"mongodb://localhost:27017/tridmark"
+        uri: "mongodb://localhost:27017/Coligo"
     },
     JWTKEY: "TRIDMARK_JWT",
-    SESSION_SECRET: 'Bb123456+',
+    SESSION_SECRET: 'Bb12scddcsc3456+',
     transportFile: {
         path: 'logs',
         maxSize: '20m',
