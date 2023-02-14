22ecbee0dd5d390056bba9318f7d47f3f110ed55
Badr Helal <64813913+badr1002@users.noreply.github.com>
2023-02-14 11:00:38 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,8 +2,7 @@
 module.exports = {
     
     db: {
-        uri: 'mongodb+srv://badr1002:og0vQafkZNaxwh4W@cluster0.jmhw5wr.mongodb.net/Coligo', 
-        // uri:"mongodb://localhost:27017/tridmark"
+        uri:"mongodb://localhost:27017/tridmark"
     },
     JWTKEY: "TRIDMARK_JWT",
     SESSION_SECRET: 'Bb123456+',
